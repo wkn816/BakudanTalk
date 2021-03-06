71024f2ee9f3c486094f41bd0278598c4a9e7b18
wkn816 <wkn816@gmail.com>
2021-03-06 18:43:55 +0900

    add-カウントアップimport

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="container">
     <div>
-
+        <Counter></Counter>
     </div>
   </section>
 </template>
 
 <script>
+import Counter from '~/components/Counter.vue'
+
   export default {
     components: {
-
+      Counter
     }
   }
 
