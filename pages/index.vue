c56b6db6e6d198b549f2c19ac91058ba82efbd78
wkn816 <wkn816@gmail.com>
2021-03-09 23:35:56 +0900

    update-インデント変更

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,41 @@
   <section class="container">
     <div>
       <div v-for="count in 1">
-          <div v-if="$store.state.counter.counter % 15 === 0" v-bind:style="fizzBuzzStyle">FizzBuzz</div>
-          <div v-else-if="$store.state.counter.counter % 5 === 0" v-bind:style="fizzStyle">Fizz </div>
-          <div v-else-if="$store.state.counter.counter % 3 === 0" v-bind:style="BuzzStyle">Buzz </div>
-          <div v-else>{{ count }}</div>
-        </div>
+        <div v-if="$store.state.counter.counter % 15 === 0" v-bind:style="fizzBuzzStyle">FizzBuzz</div>
+        <div v-else-if="$store.state.counter.counter % 5 === 0" v-bind:style="fizzStyle">Fizz</div>
+        <div v-else-if="$store.state.counter.counter % 3 === 0" v-bind:style="BuzzStyle">Buzz</div>
+        <div v-else>{{ count }}</div>
+      </div>
 
-        <Counter>
-          <!-- {{ counter }} -->
-
-        </Counter>
+      <Counter></Counter>
     </div>
   </section>
 </template>
 
 <script>
-import Counter from '~/components/Counter.vue'
+import Counter from "~/components/Counter.vue";
 
-  export default {
-    components: {
-      Counter
+export default {
+  data() {
+    return {
+      count: "",
+      fizzBuzzStyle: "",
+      fizzStyle: "",
+      BuzzStyle: "",
+    };
+  },
+  components: {
+    Counter
+  },
+  module() {
+    if (this.$store.state.counter.counter % 15 === 0) {
+      console.log("15の倍数");
     }
-
+    return {
+      fizzBuzzStyle: "",
+      fizzStyle: "",
+      BuzzStyle: "",
+      };
   }
-
+};
 </script>
