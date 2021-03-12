0c628bb527a8b44b3dc19de741716c7aa8c8dc1f
wkn816 <wkn816@gmail.com>
2021-03-12 23:59:59 +0900

    add-お題のコンポーネント追加

---
+++ components/Topic.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h2>{{ topic }}</h2>
+    <button
+      @click="countUp"
+    >+1</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      topic : "aaa"
+    }
+  },
+  // methods:{
+  //   async countUp(){
+  //     this.$store.commit('counter/countUp');
+  //     if (this.$store.state.counter.counter % 15 === 0) {
+  //     console.log("15の倍数を表示");
+  //   }
+  //   }
+  // }
+}
+</script>>
--- components/counter.vue
+++ components/counter.vue
@@ -5,10 +5,13 @@
     <button
       @click="countUp"
     >+1</button>
+    <Topic />
   </section>
 </template>
 
 <script>
+  import Topic from "~/components/Topic.vue";
+
 export default {
   data: function(){
     return{
@@ -19,6 +22,7 @@ export default {
     async countUp(){
       this.$store.commit('counter/countUp');
       if (this.$store.state.counter.counter % 15 === 0) {
+        
       console.log("15の倍数を表示");
     }
     }
