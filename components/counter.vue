eeae67fec7fe7aad7eea1d4ef6d663e3a6720116
wkn816 <wkn816@gmail.com>
2021-03-15 20:38:15 +0900

    update-爆発gif読み込み

--- components/counter.vue
+++ components/counter.vue
@@ -4,15 +4,16 @@
     counter: {{$store.state.counter.counter}}
     <button @click="countUp">+1</button>
     <p v-if="topic" class="topic">{{ topic }}</p>
+    <img :src="image_src" v-if="topic">
   </section>
 </template>
-
 <script>
 export default {
   data: function() {
     return {
       title : "BakudanTalk",
       topic : "",
+      image_src: require("../assets/bakuhatsu.gif"),
     };
   },
   methods: {
@@ -23,7 +24,11 @@ export default {
           return
       console.log("15の倍数を表示");
       }
-    }
-  }
+    },
+  },
+
 };
+
 </script>
+<style>
+</style>
