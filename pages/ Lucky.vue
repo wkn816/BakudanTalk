5c7165d9b0cc5708398f4b812ad287d18119e6ed
wkn816 <wkn816@gmail.com>
2021-03-21 21:26:58 +0900

    update-15の倍数の時の爆発ページ追加

---
+++ pages/ Lucky.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+<h1>
+</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      count: "",
+    };
+  },
+  components: {
+  },
+  module() {
+    if (this.$store.state.counter.counter % 15 === 0) {
+      console.log("15の倍数を表示");
+    }
+  }
+};
+</script>
