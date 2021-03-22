a8aba9cf149982588fd5428e2607f956789381ec
wkn816 <wkn816@gmail.com>
2021-03-22 20:27:44 +0900

    fix-不要な記述削除

--- pages/ Lucky.vue
+++ pages/ Lucky.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
 <h1>爆弾話をする</h1>
+        <!-- <img :src="image_src1" v-bind="attrs" v-on="on" class="button"> -->
     </div>
 </template>
 
@@ -9,6 +10,7 @@
 export default {
   data() {
     return {
+      // image_src: require("../assets/bakuhatsu.gif"),
     };
   },
   components: {
