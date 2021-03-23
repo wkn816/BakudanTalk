3581fe8ddfd51016ea1e58bb8b8c83c374d9354e
wkn816 <wkn816@gmail.com>
2021-03-23 23:40:34 +0900

    add-爆弾gif追加

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
 <h1>爆弾話をする</h1>
-        <!-- <img :src="image_src1" v-bind="attrs" v-on="on" class="button"> -->
+    <img :src="image_src">
     </div>
 </template>
 
@@ -10,7 +10,7 @@
 export default {
   data() {
     return {
-      // image_src: require("../assets/bakuhatsu.gif"),
+      image_src: require("../assets/bakuhatsu.gif"),
     };
   },
   components: {
