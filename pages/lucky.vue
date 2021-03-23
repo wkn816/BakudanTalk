94a7300fbd244e3f73e57cfe3538ca2140a71d24
wkn816 <wkn816@gmail.com>
2021-03-24 00:39:19 +0900

    update-3秒後にconsole表示

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -1,21 +1,28 @@
 <template>
-    <div>
-<h1>爆弾話をする</h1>
-    <img :src="image_src">
-    </div>
+  <div>
+    <h1>爆弾話をする</h1>
+    <img :src="image_src" />
+  </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       image_src: require("../assets/bakuhatsu.gif"),
+      // image_src: ""
     };
   },
-  components: {
-  },
-  module() {
+  components: {},
+  module() {},
+  methods: {},
+  async fetch() {
+    var fn = function() {
+      this.image_src = "";
+      console.log("3秒経過しました");
+    };
+    var tm = 3000;
+    setTimeout(fn, tm);
   }
 };
 </script>
