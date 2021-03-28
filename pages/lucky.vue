347b6004c07f6443a7d29fa5a247c953dc65d0ee
wkn816 <wkn816@gmail.com>
2021-03-28 21:42:32 +0900

    fix-ランダムで文字を表示する

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>爆弾話をする</h1>
+    <h1>爆発</h1>
     <img :src="image_src" />
+    <v-card>
+      <h1>Talkする</h1>
+      {{ messages }}
+    </v-card>
   </div>
 </template>
 
@@ -10,12 +14,17 @@ export default {
   data() {
     return {
       image_src: true,
+      messages: "",
     };
   },
   async fetch(){
     this.image_src = require("../assets/bakuhatsu.gif")
       setTimeout(() => {this.image_src = false;}, 4000);
       // setTimeoutで3000ms後にshowをfalseにする
+
+    const messages = ['あああ','いいい','ううう'];
+    const messageNo = Math.floor( Math.random() * messages.length);
+    console.log(messages[messageNo]);
     }
 };
 </script>
