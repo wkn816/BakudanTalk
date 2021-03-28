37e46573b6b8b3f706a29e3d9e1a80c071cfac70
wkn816 <wkn816@gmail.com>
2021-03-29 00:59:24 +0900

    add-お題を追加しました

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -22,7 +22,15 @@ export default {
       setTimeout(() => {this.image_src = false;}, 4000);
       // setTimeoutで3000ms後にshowをfalseにする
 
-    const messages = ['あああ','いいい','ううう'];
+    const messages = ['今の趣味にハマったきっかけは？',
+                      '新たに始めたい趣味について聞かせてください',
+                      '新入社員時代に失敗してしまったこと',
+                      '仕事orプライベートで直面している悩み',
+                      '今までで一番思い出に残っているクリスマスはいつ？キラキラエピソードも！',
+                      '最近感じたハッピーなこと',
+                      '最近感じたアンハッピーなこと',
+                      '異性を好きになるきっかけを教えてください',
+                      '異性ににアプローチする方法を教えてください'];
     const messageNo = Math.floor( Math.random() * messages.length);
     console.log(messages[messageNo]);
     console.log(messageNo);
