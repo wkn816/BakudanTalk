a764e7176553ce96f1b2e8cc652847780cba59ab
wkn816 <wkn816@gmail.com>
2021-04-09 15:48:21 +0900

    fix-お題カードの位置変更

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>爆発</h1>
+    <v-container>
+      <v-row>
+        <v-col cols="12" offset-sm="1" sm="4" offset-md="4" md="4" offset-lg="4" lg="4">
+
     <img :src="image_src" />
     <v-card v-if="test">
       <v-card-title>Let's Bakudan talk</v-card-title>
@@ -8,6 +11,9 @@
         <v-card-text>{{ test2 }}</v-card-text>
         <v-btn to="/" nuxt>もう一度</v-btn>
     </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
