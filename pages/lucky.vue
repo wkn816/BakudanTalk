4e7933a9177bceab2df9450421a0e5cfea6e5830
wkn816 <wkn816@gmail.com>
2021-04-12 23:51:41 +0900

    fix-幅変更

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container>
       <v-row>
-        <v-col cols="12" offset-sm="1" sm="4" offset-md="4" md="4" offset-lg="4" lg="4">
+        <v-col cols="12" offset-sm="1" sm="4" offset-md="4" md="4" offset-lg="4" lg="5">
             <img :src="image_src" />
             <v-card v-if="test">
               <v-card-title>Let's Bakudan talk</v-card-title>
