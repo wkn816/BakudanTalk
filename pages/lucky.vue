dabb9101177f856944057d2748f6dfe48f7c9531
wkn816 <wkn816@gmail.com>
2021-04-15 16:32:02 +0900

    fix-レスポンシブ

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -2,10 +2,10 @@
   <div>
     <v-container>
       <v-row>
-        <v-col cols="12" offset-sm="1" sm="4" offset-md="4" md="4" offset-lg="4" lg="5">
+        <v-col cols="12" offset-sm="1" sm="4" offset-md="4" md="4" offset-lg="3" lg="6">
             <img :src="image_src" />
             <v-card v-if="test">
-              <v-card-title>Let's Bakudan talk</v-card-title>
+              <v-card-title class="card-title">Let's Bakudan talk</v-card-title>
               <v-card-text>{{ test }}</v-card-text>
               <v-card-text>{{ test2 }}</v-card-text>
               <v-btn to="/" nuxt>もう一度</v-btn>
@@ -57,5 +57,13 @@
 </script>
 
 <style>
-
+.card-title{
+  text-align: center;
+}
+.v-card-title{
+  text-align: center;
+}
+.v-card__title{
+  text-align: center;
+}
 </style>
