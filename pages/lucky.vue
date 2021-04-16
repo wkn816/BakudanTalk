522bbd195983652ef83e9a6bccf2c943ce9ec209
wkn816 <wkn816@gmail.com>
2021-04-17 03:03:44 +0900

    fix-リプレイボタンのレスポンシブ

--- pages/lucky.vue
+++ pages/lucky.vue
@@ -4,9 +4,9 @@
       <v-row>
         <v-col cols="12" offset-sm="1" sm="4" offset-md="2" md="8">
             <img :src="image_src" class="bakuhatsu-gif"/>
-            <v-card v-if="test" color=#2e2f3e shaped class="ma-2">
+            <v-card v-if="test" color=#2e2f3e shaped class="d-flex flex-column" height="400">
               <v-card-title class="justify-center">Let's Bakudan talk</v-card-title>
-              <v-card-text>
+              <v-card-text class="parent">
                 <p class="theme-text">{{ test }}</p>
               </v-card-text>
             </v-card>
@@ -14,9 +14,9 @@
       </v-row>
 
       <v-row>
-        <v-col cols="12" offset-sm="1" sm="4" offset-md="5" md="4">
+        <v-col cols="12" sm="12" offset-md="4" md="4">
           <v-hover v-slot:default="{ hover }">
-              <v-btn v-if="test" to="/" nuxt color=#ff8906>
+              <v-btn v-if="test" to="/" nuxt color=#ff8906 block>
               <v-icon v-text="hover ? 'mdi-restore' : 'mdi-refresh'"></v-icon>REPLAY
               </v-btn>
               </v-hover>
@@ -72,7 +72,7 @@
   text-align: center;
   font-size: 20px;
   color: #fffffe;
-}
+position: absolute;}
 .bakuhatsu-gif{
   text-align: center;
   width: 100%;
@@ -86,5 +86,16 @@
       transform: scale(1.04);
       transition-duration: 40ms;
     }
+}
+.parent {
+  /*親要素*/
+  position: relative;
+  height: 300px;
+}
+.parent p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform : translate(-50%,-50%);
 }
 </style>
