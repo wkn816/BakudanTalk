8254b1448383e3518bf0cc9ac5ac4ebc3c3bb9b2
wkn816 <wkn816@gmail.com>
2021-05-27 18:15:23 +0900

    add-スタートボタン追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,23 @@
 <section>
 <v-container>
       <v-row>
-        <v-col cols="12" offset-sm="1" sm="4" offset-md="2" md="8">
+        <v-col cols="12" offset-sm="1" sm="6" offset-md="2" md="8">
             <img :src="startlogo" class="start-logo">
+        </v-col>
+        <v-col cols="12" offset-sm="1" sm="6" offset-md="4" md="4">
+
+            <v-hover v-slot:default="{ hover }">
+              <v-btn to="/top" nuxt color=#ff8906 block>
+              <v-icon v-text="hover ? 'mdi-restore' : 'mdi-refresh'"></v-icon>START
+              </v-btn>
+              </v-hover>
+        </v-col>
+        <v-col cols="12" offset-sm="1" sm="6" offset-md="4" md="4">
+            <v-hover v-slot:default="{ hover }">
+              <v-btn to="/about" nuxt color=#ff8906 block>
+              <v-icon v-text="hover ? 'mdi-restore' : 'mdi-refresh'"></v-icon> HOW TO USE
+              </v-btn>
+              </v-hover>
         </v-col>
       </v-row>
     </v-container>
