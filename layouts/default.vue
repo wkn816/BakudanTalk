63c9632a5474f1f97a226023920ebf0aed9c43cb
wkn816 <wkn816@gmail.com>
2021-05-28 18:13:11 +0900

    fix-ドロワー削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,14 @@
 <template>
   <v-app class="bomber-all">
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
+    <nuxt-link to="/">
+      <img :src="titleLogo" class="title-logo">
+    </nuxt-link>
       <v-spacer />
     </v-app-bar>
     <v-main>
@@ -38,23 +16,7 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-footer
       :absolute="!fixed"
       app
@@ -68,25 +30,7 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-bomb',
-          title: 'Top',
-          to: '/'
-        },
-        {
-          icon: 'mdi-fire',
-          title: 'How to use',
-          to: '/about'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Bomber'
+      titleLogo: require("../assets/bomber-logo.png")
     }
   }
 }
@@ -119,5 +63,8 @@ $sp: 480px;  // スマホ
   text-align: center;
   background-color: #0f0e17;
   font-family: 'HogeHogeFont';
+}
+.title-logo{
+  width: 130px;
 }
 </style>
