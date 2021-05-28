5c065b649ca3a4a58249bcd5db65d3d6a945df6f
wkn816 <wkn816@gmail.com>
2021-05-28 18:12:58 +0900

    fix-スタート画面追加作成

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,22 @@
 <section>
 <v-container>
       <v-row>
-        <v-col cols="12" offset-sm="1" sm="6" offset-md="2" md="8">
+        <v-col cols="12" sm="12" md="12">
             <img :src="startlogo" class="start-logo">
         </v-col>
-        <v-col cols="12" offset-sm="1" sm="6" offset-md="4" md="4">
-
+        <v-col cols="12" offset-sm="2" sm="8" offset-md="4" md="4">
             <v-hover v-slot:default="{ hover }">
-              <v-btn to="/top" nuxt color=#ff8906 block>
-              <v-icon v-text="hover ? 'mdi-restore' : 'mdi-refresh'"></v-icon>START
+              <v-btn class="topbtn" to="/top" nuxt block color=#7f5af0 height="80" :ripple="{ center: true }">
+              <v-icon large v-text="hover ? 'mdi-arrow-left-thick' : ''"></v-icon>
+              <div class="top-text">START</div>
               </v-btn>
               </v-hover>
         </v-col>
-        <v-col cols="12" offset-sm="1" sm="6" offset-md="4" md="4">
+        <v-col cols="12" offset-sm="2" sm="8" offset-md="4" md="4">
             <v-hover v-slot:default="{ hover }">
-              <v-btn to="/about" nuxt color=#ff8906 block>
-              <v-icon v-text="hover ? 'mdi-restore' : 'mdi-refresh'"></v-icon> HOW TO USE
+              <v-btn to="/about" nuxt color=#2cb67d block height="80" :ripple="{ center: true }">
+              <v-icon large v-text="hover ? 'mdi-arrow-left-thick' : ''"></v-icon>
+              <div class="top-text"> HOW TO USE</div>
               </v-btn>
               </v-hover>
         </v-col>
@@ -41,9 +42,22 @@ export default {
   }
 };
 </script>
-<style>
+<style lang="scss">
+$sp: 480px;  // スマホ
+@mixin sp {
+  @media (max-width: ($sp)) {
+    @content;
+  }
+}
+
 .start-logo{
-  width: 60%;
+  width: 40%;
+  @include sp {
+    width: 300px;
+  }
+}
+.top-text{
+  font-size: 60px;
 }
 
 </style>
